fdd148473045b24d77c257bb7bc9ab1101350d96
Christian Berendt <berendt@betacloud-solutions.de>
2018-10-07 20:39:20 +0200

    Use logging

--- src/images.py
+++ src/images.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import logging
 import os
 import shutil
 import subprocess
@@ -11,6 +12,7 @@ import os_client_config
 import requests
 import shade
 
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
 shade.simple_logging(debug=os.environ.get('DEBUG', False))
 
 CLOUD= os.environ.get('CLOUD', 'images')
@@ -46,12 +48,12 @@ for image in images:
 
     for required_key in REQUIRED_KEYS:
         if required_key not in image:
-            print("'%s' lacks the necessary key %s" % (image['name'], required_key))
+            logging.info("'%s' lacks the necessary key %s" % (image['name'], required_key))
             skip = True
     if skip:
         continue
 
-    print("Processing '%s'" % image['name'])
+    logging.info("Processing '%s'" % image['name'])
 
     # check existence
     existence = image['name'] in cloud_images
@@ -61,13 +63,13 @@ for image in images:
         # check image url
 
         r = requests.head(image['url'])
-        print("Test URL %s: %s" % (image['url'], r.status_code))
+        logging.info("Test URL %s: %s" % (image['url'], r.status_code))
 
         if r.status_code not in [200, 302]:
-            print("Skipping '%s'" % image['name'])
+            logging.info("Skipping '%s'" % image['name'])
             continue
 
-        print("Creating import task '%s'" % image['name'])
+        logging.info("Creating import task '%s'" % image['name'])
         input = {
             'import_from_format': image['format'],
             'import_from': image['url'],
@@ -85,7 +87,7 @@ for image in images:
             try:
                 status = glance.tasks.get(t.id).status
                 if status not in ['failure', 'success']:
-                    print("Waiting for task %s" % t.id)
+                    logging.info("Waiting for task %s" % t.id)
                     time.sleep(10.0)
                 else:
                     break
@@ -98,42 +100,42 @@ for image in images:
             cloud_images = get_images(cloud)
 
     if existence or (not existence and status == 'success'):
-        print("Checking parameters of '%s'" % image['name'])
+        logging.info("Checking parameters of '%s'" % image['name'])
 
         cloud_image = cloud_images[image['name']]
         properties = cloud_image.properties
 
         if 'min_disk' in image and image['min_disk'] != cloud_image.min_disk:
-            print("Setting min_disk: %s != %s" % (image['min_disk'], cloud_image.min_disk))
+            logging.info("Setting min_disk: %s != %s" % (image['min_disk'], cloud_image.min_disk))
             cloud.update_image_properties(name_or_id=cloud_image.id, min_disk=image['min_disk'])
 
         if 'min_ram' in image and image['min_ram'] != cloud_image.min_ram:
-            print("Setting min_ram: %s != %s" % (image['min_ram'], cloud_image.min_ram))
+            logging.info("Setting min_ram: %s != %s" % (image['min_ram'], cloud_image.min_ram))
             cloud.update_image_properties(name_or_id=cloud_image.id, min_ram=image['min_ram'])
 
         for property in properties:
             if property in image['meta']:
                 if image['meta'][property] != properties[property]:
-                    print("Setting %s: %s != %s" % (property, properties[property], image['meta'][property]))
+                    logging.info("Setting %s: %s != %s" % (property, properties[property], image['meta'][property]))
                     glance.images.update(cloud_image.id, **{property: str(image['meta'][property])})
             elif property not in ['self', 'schema']:
                 # FIXME: handle deletion of properties
-                print("Deleting %s" % (property))
+                logging.info("Deleting %s" % (property))
 
         for property in image['meta']:
             if property not in properties:
-                print("Setting %s: %s" % (property, image['meta'][property]))
+                logging.info("Setting %s: %s" % (property, image['meta'][property]))
                 glance.images.update(cloud_image.id, **{property: str(image['meta'][property])})
 
-        print("Checking status of '%s'" % image['name'])
+        logging.info("Checking status of '%s'" % image['name'])
         if cloud_image.status != image['status'] and image['status'] == 'deactivated':
-            print("Deactivating image '%s'" % image['name'])
+            logging.info("Deactivating image '%s'" % image['name'])
             glance.images.deactivate(cloud_image.id)
         elif cloud_image.status != image['status'] and image['status'] == 'active':
-            print("Reactivating image '%s'" % image['name'])
+            logging.info("Reactivating image '%s'" % image['name'])
             glance.images.reactivate(cloud_image.id)
 
-        print("Checking visibility of '%s'" % image['name'])
+        logging.info("Checking visibility of '%s'" % image['name'])
         if cloud_image.visibility != image['visibility']:
-            print("Set visibility of '%s' to '%s'" % (image['name'], image['visibility']))
+            logging.info("Set visibility of '%s' to '%s'" % (image['name'], image['visibility']))
             glance.images.update(cloud_image.id, visibility=image['visibility'])
