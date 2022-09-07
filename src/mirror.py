46223fd4b0b040f3883b0dd8d6da3a5cc24353ff
Simon Hensel <simon.hensel@inovex.de>
2022-09-07 14:17:49 +0200

    Switch to typer as CLI for all scripts
    * Remove oslo.config from requirements.txt
    * Add tox environment for table.py
    
    Signed-off-by: Simon Hensel <simon.hensel@inovex.de>

--- src/mirror.py
+++ src/mirror.py
@@ -3,95 +3,102 @@
 import logging
 import os
 import shutil
-import sys
-from urllib.parse import urlparse
-
-from oslo_config import cfg
-from minio import Minio
-from minio.error import S3Error
-from os import listdir
-from os.path import isfile, join
 import patoolib
 import requests
 import yaml
+import typer
+from urllib.parse import urlparse
 
-PROJECT_NAME = 'mirror'
-CONF = cfg.CONF
-opts = [
-    cfg.BoolOpt('debug', help='Enable debug logging', default=False),
-    cfg.BoolOpt('dry-run', help='Do not really do anything', default=False),
-    cfg.StrOpt('images', help='Path to the folder with the image files', default='etc/images/'),
-    cfg.StrOpt('minio-access-key', help='Minio access key'),
-    cfg.StrOpt('minio-secret-key', help='Minio secret key'),
-    cfg.StrOpt('minio-server', help='Minio server', default='minio.services.osism.tech'),
-    cfg.StrOpt('minio-bucket', help='Minio bucket', default='openstack-image-manager')
-]
-CONF.register_cli_opts(opts)
-CONF(sys.argv[1:], project=PROJECT_NAME)
-if CONF.debug:
-    level = logging.DEBUG
-    logging.getLogger("paramiko").setLevel(logging.DEBUG)
-else:
-    level = logging.INFO
-    logging.getLogger("paramiko").setLevel(logging.WARNING)
-logging.basicConfig(format='%(asctime)s - %(message)s', level=level, datefmt='%Y-%m-%d %H:%M:%S')
+from minio import Minio
+from minio.error import S3Error
+from munch import Munch
+from os import listdir
+from os.path import isfile, join
 
-onlyfiles = []
-for f in listdir(CONF.images):
-    if isfile(join(CONF.images, f)):
-        onlyfiles.append(f)
 
-all_images = []
-for file in onlyfiles:
-    with open(join(CONF.images, file)) as fp:
-        data = yaml.load(fp, Loader=yaml.SafeLoader)
-        images = data.get('images')
-        for image in images:
-            all_images.append(image)
+app = typer.Typer(add_completion=False)
 
-client = Minio(
-    CONF.minio_server,
-    access_key=CONF.minio_access_key,
-    secret_key=CONF.minio_secret_key
-)
 
-for image in all_images:
-    for version in image['versions']:
-        if 'source' not in version:
-            continue
+@app.command()
+def main(
+        debug: bool = typer.Option(False, '--debug', help='Enable debug logging'),
+        dry_run: bool = typer.Option(False, '--dry-run', help='Do not perform any changes'),
+        images: str = typer.Option('etc/images/', help='Path to the directory containing all image files'),
+        minio_access_key: str = typer.Option(None, help='Minio access key'),
+        minio_secret_key: str = typer.Option(None, help='Minio secret key'),
+        minio_server: str = typer.Option('minio.services.osism.tech', help='Minio server'),
+        minio_bucket: str = typer.Option('openstack-image-manager', help='Minio bucket')
+):
+    CONF = Munch.fromDict(locals())
 
-        logging.debug("source: %s" % version['source'])
+    if CONF.debug:
+        level = logging.DEBUG
+        logging.getLogger("paramiko").setLevel(logging.DEBUG)
+    else:
+        level = logging.INFO
+        logging.getLogger("paramiko").setLevel(logging.WARNING)
+    logging.basicConfig(format='%(asctime)s - %(message)s', level=level, datefmt='%Y-%m-%d %H:%M:%S')
 
-        path = urlparse(version['source'])
+    onlyfiles = []
+    for f in listdir(CONF.images):
+        if isfile(join(CONF.images, f)):
+            onlyfiles.append(f)
 
-        dirname = "%s/%s" % (image['shortname'], version['version'])
-        filename, fileextension = os.path.splitext(os.path.basename(path.path))
+    all_images = []
+    for file in onlyfiles:
+        with open(join(CONF.images, file)) as fp:
+            data = yaml.load(fp, Loader=yaml.SafeLoader)
+            images = data.get('images')
+            for image in images:
+                all_images.append(image)
 
-        if fileextension not in ['.bz2', '.zip', '.xz']:
-            filename += fileextension
+    client = Minio(
+        CONF.minio_server,
+        access_key=CONF.minio_access_key,
+        secret_key=CONF.minio_secret_key
+    )
 
-        logging.debug("dirname: %s" % dirname)
-        logging.debug("filename: %s" % filename)
+    for image in all_images:
+        for version in image['versions']:
+            if 'source' not in version:
+                continue
 
-        try:
-            client.stat_object(CONF.minio_bucket, os.path.join(dirname, filename))
-            logging.info("'%s' available in '%s'" % (filename, dirname))
-        except S3Error:
-            logging.info("'%s' not yet available in '%s'" % (filename, dirname))
+            logging.debug("source: %s" % version['source'])
 
-            if not CONF.dry_run:
-                logging.info("Downloading '%s'" % version['source'])
-                response = requests.get(version['source'], stream=True)
-                with open(os.path.basename(path.path), 'wb') as fp:
-                    shutil.copyfileobj(response.raw, fp)
-                del response
+            path = urlparse(version['source'])
 
-                if fileextension in ['.bz2', '.zip', '.xz']:
-                    logging.info("Decompressing '%s'" % os.path.basename(path.path))
-                    patoolib.extract_archive(os.path.basename(path.path), outdir='.')
-                    os.remove(os.path.basename(path.path))
+            dirname = "%s/%s" % (image['shortname'], version['version'])
+            filename, fileextension = os.path.splitext(os.path.basename(path.path))
 
-                logging.info("Uploading '%s' to '%s'" % (filename, dirname))
+            if fileextension not in ['.bz2', '.zip', '.xz']:
+                filename += fileextension
 
-                client.fput_object(CONF.minio_bucket, os.path.join(dirname, filename), filename)
-                os.remove(filename)
+            logging.debug("dirname: %s" % dirname)
+            logging.debug("filename: %s" % filename)
+
+            try:
+                client.stat_object(CONF.minio_bucket, os.path.join(dirname, filename))
+                logging.info("'%s' available in '%s'" % (filename, dirname))
+            except S3Error:
+                logging.info("'%s' not yet available in '%s'" % (filename, dirname))
+
+                if not CONF.dry_run:
+                    logging.info("Downloading '%s'" % version['source'])
+                    response = requests.get(version['source'], stream=True)
+                    with open(os.path.basename(path.path), 'wb') as fp:
+                        shutil.copyfileobj(response.raw, fp)
+                    del response
+
+                    if fileextension in ['.bz2', '.zip', '.xz']:
+                        logging.info("Decompressing '%s'" % os.path.basename(path.path))
+                        patoolib.extract_archive(os.path.basename(path.path), outdir='.')
+                        os.remove(os.path.basename(path.path))
+
+                    logging.info("Uploading '%s' to '%s'" % (filename, dirname))
+
+                    client.fput_object(CONF.minio_bucket, os.path.join(dirname, filename), filename)
+                    os.remove(filename)
+
+
+if __name__ == '__main__':
+    app()
--- src/table.py
+++ src/table.py
@@ -1,39 +1,45 @@
-import sys
-
-from oslo_config import cfg
-from os import listdir
-from os.path import isfile, join
 import tabulate
+import typer
 import yaml
 
-PROJECT_NAME = 'table'
-CONF = cfg.CONF
-opts = [
-    cfg.StrOpt('images', help='Path to the folder with the image files', default='etc/images/')
-]
-CONF.register_cli_opts(opts)
-CONF(sys.argv[1:], project=PROJECT_NAME)
+from munch import Munch
+from os import listdir
+from os.path import isfile, join
 
-onlyfiles = []
-for f in listdir(CONF.images):
-    if isfile(join(CONF.images, f)):
-        onlyfiles.append(f)
 
-all_images = []
-for file in onlyfiles:
-    with open(join(CONF.images, file)) as fp:
-        data = yaml.load(fp, Loader=yaml.SafeLoader)
-        images = data.get('images')
-        for image in images:
-            all_images.append(image)
+app = typer.Typer(add_completion=False)
 
-data = []
-for image in all_images:
-    data.append([image['name'], image['login'], image.get('password', '')])
 
-result = tabulate.tabulate(
-    sorted(data),
-    headers=['Name', 'Login user', 'Password'],
-    tablefmt="rst"
-)
-print(result)
+@app.command()
+def main(
+    images: str = typer.Option('etc/images/', help='Path to the directory containing all image files')
+):
+    CONF = Munch.fromDict(locals())
+
+    onlyfiles = []
+    for f in listdir(CONF.images):
+        if isfile(join(CONF.images, f)):
+            onlyfiles.append(f)
+
+    all_images = []
+    for file in onlyfiles:
+        with open(join(CONF.images, file)) as fp:
+            data = yaml.load(fp, Loader=yaml.SafeLoader)
+            images = data.get('images')
+            for image in images:
+                all_images.append(image)
+
+    data = []
+    for image in all_images:
+        data.append([image['name'], image['login'], image.get('password', '')])
+
+    result = tabulate.tabulate(
+        sorted(data),
+        headers=['Name', 'Login user', 'Password'],
+        tablefmt="rst"
+    )
+    print(result)
+
+
+if __name__ == '__main__':
+    app()
