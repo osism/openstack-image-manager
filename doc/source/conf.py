ba43a4ba9c1d015e9cae5fa390915a066c3d0abf
Christian Berendt <berendt@osism.tech>
2022-10-19 15:46:04 +0200

    doc: use sphinx material theme (#358)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,39 +1,39 @@
 def setup(app):
-    app.add_css_file('css/custom.css')
+    app.add_css_file("css/custom.css")
 
 
 extensions = [
-  'sphinx.ext.autosectionlabel', 'sphinxcontrib.blockdiag',
-  'sphinxcontrib.nwdiag', 'sphinx.ext.todo', 'sphinx_fontawesome'
-  ]
-source_suffix = '.rst'
-master_doc = 'index'
-project = u''
-copyright = u'2022, OSISM GmbH'
-author = u'OSISM GmbH'
-version = u''
-release = u''
-language = 'en'
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.todo",
+    "sphinx_fontawesome",
+]
+source_suffix = ".rst"
+master_doc = "index"
+project = ""
+copyright = "2022, OSISM GmbH"
+author = "OSISM GmbH"
+version = ""
+release = ""
+language = "en"
 exclude_patterns = []
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 todo_include_todos = True
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_material"
 html_show_sphinx = False
 html_show_sourcelink = False
 html_show_copyright = True
-htmlhelp_basename = 'documentation-image-manager'
+htmlhelp_basename = "openstack-image-manager"
 html_theme_options = {
-    'display_version': False,
-    # 'canonical_url': 'https://docs.osism.de/',
-    'style_external_links': True,
-    'logo_only': True,
-    'prev_next_buttons_location': None
+    "nav_title": "OSISM OpenStack Image Manager",
+    "color_primary": "blue",
+    "color_accent": "light-blue",
+    "globaltoc_depth": 3,
+    "globaltoc_collapse": True,
 }
-html_context = {
-    'display_github': True,
-    'github_user': 'osism',
-    'github_repo': 'openstack-image-manager',
-    'github_version': 'main',
-    'conf_py_path': '/source/'
+html_context = {}
+html_title = "OSISM OpenStack Image Manager"
+html_logo = "images/logo.png"
+html_sidebars = {
+    "**": ["logo-text.html", "globaltoc.html", "localtoc.html", "searchbox.html"]
 }
 latex_elements = {}
