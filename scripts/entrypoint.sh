3532de3df17b4abf2e23e2f6dcbe49686b44d277
Andreas Schrogl <ansc@uhurutec.com>
2024-07-11 10:02:40 +0200

    changed entypoint.sh
    Signed-off-by: Andreas Schrogl <ansc@uhurutec.com>

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,9 +3,11 @@
 SCHEDULE=${SCHEDULE:-0 1 * * 7}
 CLOUD=${CLOUD:-openstack}
 
+mkdir /oim/images
+
 env >> /etc/environment
 # build cronjob entry
-croncmd="git clone --depth 1 https://github.com/osism/openstack-image-manager.git /openstack-image-manager; rm /etc/images/*.yml; cp /openstack-image-manager/etc/images/* /etc/images; rm -rf /openstack-image-manager &&  /usr/local/bin/openstack-image-manager --cloud $CLOUD --images /etc/images/ --filter \"$FILTER\" > /proc/1/fd/1 2>/proc/1/fd/2"
+croncmd="git clone --depth 1 https://github.com/osism/openstack-image-manager.git /oim/src; rm /oim/images/*; cp /oim/src/etc/images/* /oim/images; rm -rf /oim/src &&  /usr/local/bin/openstack-image-manager --cloud $CLOUD --images /oim/images/ --filter \"$FILTER\" > /proc/1/fd/1 2>/proc/1/fd/2"
 
 echo "SHELL=/bin/bash" >> /etc/cron.d/openstack-image-manager
 echo "BASH_ENV=/etc/environment" >> /etc/cron.d/openstack-image-manager
